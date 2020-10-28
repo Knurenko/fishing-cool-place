d93b94fc729e162dcec8683d5e539e8d718aacc7
AndrRum <andrey_rumiantsev95@mail.ru>
2020-10-28 14:57:56 +0200

    #6

--- App.js
+++ App.js
@@ -1,9 +1,27 @@
+import 'react-native-gesture-handler'
 import React from 'react'
-import HelloWorld from './src/screens/HW'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+
+import HW from './src/screens/HW'
+import HA from './src/screens/HA' 
+
+const Stack = createStackNavigator();
 
 export default function App () {
-    return (
-        <HelloWorld/>
-    )
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Hello, its my first screen"
+          component={HW}
+        />
+        <Stack.Screen 
+          name="Hola, its my second screen" 
+          component={HA} 
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
-
--- src/screens/HW.js
+++ src/screens/HW.js
@@ -1,29 +1,34 @@
 import React from 'react'
-import { StyleSheet, Text, View, Button, Alert } from 'react-native'
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native'
 
-export default function App () {
+
+
+export default function App ({navigation}) {
   return (
     <View style = {styles.container}>
-      <Text style = { { color: '#fff', fontWeight: 'bold', fontSize: 30 } }> Hello World! </Text> 
-      <View style={styles.button}>
-      <Button 
-        title="Go to another screen"
-        color="#800000"
-        onPress = {() => Alert.alert('I cant do this right now, I dont know how :c')}
-/>
-    </View>
+      <Text style = { { color: '#B22222', fontWeight: 'bold', fontSize: 45, textShadowColor: 'rgba(0, 0, 0, 0.75)', 
+      textShadowOffset: {width: -1, height: 1},  textShadowRadius: 10 } }> Hello, World! </Text> 
+        <TouchableOpacity style={styles.button}>
+          <Button 
+            title="Go to another screen"
+            color="#800000"
+            onPress = {() => navigation.navigate('Hola, its my second screen')}
+          />
+        </TouchableOpacity>
     </View>
   )
 }
 
+
 const styles = StyleSheet.create ({
   container: {
     flex: 1,
-    backgroundColor: '#000',
+    backgroundColor: '#FFDAB9',
     alignItems: 'center',
     justifyContent: 'space-around'
   },
   button: {
-    alignSelf: 'stretch'
-  }
+    alignSelf: 'stretch',
+  },
+  
 })
